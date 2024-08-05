de0c5a741c88d46b3e6c87e4c5d1d880fa34efd3
Ku06 <k.u.0115.009@gmail.com>
2024-08-05 14:26:54 +0900

    Update read.php

--- kadai20240704/20240704php01/read.php
+++ kadai20240704/20240704php01/read.php
@@ -1,7 +1,17 @@
 <?php
-$a = 1;
-$b = 2;
-$c = "abc";
-echo $c.$c;
+
+$file = fopen('data.csv', 'r');
+
+//繰り返し処理
+while(!feof($file)){
+    //一行ずつ出力
+    $line=fgets($file);
+    echo $line;
+    //表示
+
+}
+
+//閉じる
+fclose($file);
 
 ?>
